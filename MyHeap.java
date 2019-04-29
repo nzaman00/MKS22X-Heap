4b64c7e90e8a09a77f59791c80b91d7059bc9e13
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-04-29 10:01:54 -0400

    Update MyHeap.java

--- MyHeap.java
+++ MyHeap.java
@@ -1,3 +1,8 @@
-public class MyHeap{
-
+import java.util.*;
+public class MyHeap {
+  private static void swap(int[] data, int indx1, int indx2) {
+    int temp = data[indx2];
+    data[indx2] = data[indx1];
+    data[indx1] = temp;
+  }
 }
